453f49fa8e10d267298b870f8b213ac1c6a45ef3
akhihaani <akhihaani@gmail.com>
2026-06-26 17:42:52 +0100

    Removed comments from vriables.tf files

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,19 +1,16 @@
 variable "region" {
   type        = string
-  description = "AWS region to deploy into"
-  # no default → Terraform will demand a value 
+  description = "AWS region to deploy into" 
 }
 
 variable "account_id" {
   type        = string
   description = "AWS account ID"
-  # no default → Terraform will demand a value
 }
 
 variable "domain" {
   type        = string
   description = "Domain"
-  # no default → Terraform will demand a value
 }
 
 variable "github_repo" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,16 @@
 variable "region" {
   type        = string
   description = "AWS region to deploy into"
-  # no default → Terraform will demand a value 
 }
 
 variable "account_id" {
   type        = string
   description = "AWS account ID"
-  # no default → Terraform will demand a value
 }
 
 variable "domain" {
   type        = string
   description = "Domain"
-  # no default → Terraform will demand a value
 }
 
 variable "ecr_repo" {
